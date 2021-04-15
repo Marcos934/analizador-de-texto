21f8cc82e00265a8fae4b639a7722517a68bc827
Marcos Mulinari <marcos.mulinari97@gmail.com>
2021-04-15 02:50:40 +0000

    Analise de tratamento de envio e tratamento de erros de frases vazias

--- src/components/EntradaAnalise.vue
+++ src/components/EntradaAnalise.vue
@@ -4,26 +4,45 @@
       class="textarea is-info textarea is-medium"
       placeholder="Amo animais"
       v-model="frase"
+      maxlength="100"
     ></textarea>
     <div class="buttons">
-      <button @click="Analize" class="button is-link mt-2">Analizar Texto</button>
+      <button @click="Analize" class="button is-link mt-2">
+        Analizar Texto
+      </button>
     </div>
   </div>
-  <div class="mt-4 container is-max-desktop">
-    <p class="title is-2">Resultado:</p>
-    <label class="title is-6">Negativo: 80%</label>
+
+  <div class="mt-4" v-if="resposta == 'enviado'">
+    <div class="has-text-centered">Analizando aguarde...</div>
+    <progress class="progress is-large is-info" max="100">60%</progress>
+  </div>
+  <div
+    class="mt-4 container is-max-desktop"
+    v-else-if="resposta == 'analizado'"
+  >
+    <p class="title is-2">Resultado: {{ resultado }}</p>
+    <label class="title is-6">Negativo: {{ sentimentos.negativo }}%</label>
     <progress
       class="progress is-medium is-danger"
-      value="0.80"
+      :value="sentimentos.negativo"
       max="1"
     ></progress>
-    <label class="title is-6">Neutro: 80% </label>
-    <progress class="progress is-medium is-warning" value="0.80" max="1">
+    <label class="title is-6">Neutro: {{ sentimentos.neutro }}% </label>
+    <progress
+      class="progress is-medium is-warning"
+      :value="sentimentos.neutro"
+      max="1"
+    >
       80%
     </progress>
 
-    <label class="title is-6">Positivo: 80% </label>
-    <progress class="progress is-medium is-success" value="0.80" max="1">
+    <label class="title is-6">Positivo: {{ sentimentos.positivo }}% </label>
+    <progress
+      class="progress is-medium is-success"
+      :value="sentimentos.positivo"
+      max="1"
+    >
       1
     </progress>
   </div>
@@ -31,27 +50,33 @@
   <hr />
 </template>
 <script>
-
 export default {
   data() {
     return {
+      resposta: null,
       frase: "",
       resultado: null,
-      sentimentos:{
-          positivo: null,
-          neutro: null,
-          negativo: null
-      }
+      sentimentos: {
+        positivo: 0,
+        neutro: 0,
+        negativo: 0,
+      },
     };
   },
   methods: {
-    Analize() {
+    async Analize() {
+      if (this.verificaTexto() == false) {
+        return 0;
+      }
+
+      this.dispResposta("enviado");
+
       const {
         TextAnalyticsClient,
         AzureKeyCredential,
       } = require("@azure/ai-text-analytics");
-        const key = process.env.VUE_APP_KEY;
-        const endpoint = process.env.VUE_APP_ENDPOINT;
+      const key = process.env.VUE_APP_KEY;
+      const endpoint = process.env.VUE_APP_ENDPOINT;
 
       const textAnalyticsClient = new TextAnalyticsClient(
         endpoint,
@@ -95,11 +120,28 @@ export default {
         // }
         // languageDetection(textAnalyticsClient);
       }
-      sentimentAnalysis(textAnalyticsClient);
+
+      await sentimentAnalysis(textAnalyticsClient);
+      this.dispResposta("analizado");
     },
-    teste(){
-      
-    }
+    dispResposta(valor) {
+      this.resposta = valor;
+      console.log(valor);
+    },
+    verificaTexto() {
+      if (this.frase.length == 0 || this.frase == "") {
+        alert("A frase para análise não pode ser vazia");
+        return false;
+      } else if (this.frase.length > 100) {
+        alert("A frase deve conter no máximo 100 caracteres");
+        return false;
+      } else {
+        return true;
+      }
+    },
+    teste() {
+      return alert();
+    },
   },
 };
 </script>
